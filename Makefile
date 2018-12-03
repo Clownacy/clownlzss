c7f9074276daea0af0202a57a46931b665f8ee4e
Clownacy <Clownacy@users.noreply.github.com>
2018-12-03 17:29:41 +0000

    Added Kosinski+

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
 CFLAGS := -O2 -s -Wall -Wextra -fno-ident -flto
 
-all: kos comp
+all: kos kosp comp
 
 kos: maink.c kosinski.c memory_stream.c
+	$(CC) $(CFLAGS) -o $@ $^ $(LDFLAGS) $(LIBS)
+
+kosp: mainkp.c kosinskiplus.c memory_stream.c
 	$(CC) $(CFLAGS) -o $@ $^ $(LDFLAGS) $(LIBS)
 
 comp: main.c comper.c memory_stream.c
--- kosinskiplus.c
+++ kosinskiplus.c
@@ -1,4 +1,4 @@
-#include "kosinski.h"
+#include "kosinskiplus.h"
 
 #include <stdbool.h>
 #include <stddef.h>
@@ -6,20 +6,19 @@
 #include "clownlzss.h"
 #include "memory_stream.h"
 
-#define TOTAL_DESCRIPTOR_BITS 16
+#define TOTAL_DESCRIPTOR_BITS 8
 
 static MemoryStream *output_stream;
 static MemoryStream *match_stream;
 
-static unsigned short descriptor;
+static unsigned char descriptor;
 static unsigned int descriptor_bits_remaining;
 
 static void FlushData(void)
 {
-	descriptor >>= descriptor_bits_remaining;
+	descriptor <<= descriptor_bits_remaining;
 
-	MemoryStream_WriteByte(output_stream, descriptor & 0xFF);
-	MemoryStream_WriteByte(output_stream, descriptor >> 8);
+	MemoryStream_WriteByte(output_stream, descriptor);
 
 	const size_t match_buffer_size = MemoryStream_GetIndex(match_stream);
 	unsigned char *match_buffer = MemoryStream_GetBuffer(match_stream);
@@ -34,13 +33,6 @@ static void PutMatchByte(unsigned char byte)
 
 static void PutDescriptorBit(bool bit)
 {
-	--descriptor_bits_remaining;
-
-	descriptor >>= 1;
-
-	if (bit)
-		descriptor |= 1 << (TOTAL_DESCRIPTOR_BITS - 1);
-
 	if (descriptor_bits_remaining == 0)
 	{
 		FlushData();
@@ -48,6 +40,12 @@ static void PutDescriptorBit(bool bit)
 		descriptor_bits_remaining = TOTAL_DESCRIPTOR_BITS;
 		MemoryStream_Reset(match_stream);
 	}
+
+	--descriptor_bits_remaining;
+
+	descriptor <<= 1;
+
+	descriptor |= bit;
 }
 
 static void DoLiteral(unsigned char value, void *user)
@@ -66,24 +64,24 @@ static void DoMatch(size_t distance, size_t length, void *user)
 	{
 		PutDescriptorBit(false);
 		PutDescriptorBit(false);
+		PutMatchByte(-distance);
 		PutDescriptorBit((length - 2) & 2);
 		PutDescriptorBit((length - 2) & 1);
-		PutMatchByte(-distance);
 	}
 	else if (length >= 3 && length <= 9)
 	{
 		PutDescriptorBit(false);
 		PutDescriptorBit(true);
+		PutMatchByte(((-distance >> (8 - 3)) & 0xF8) | ((10 - length) & 7));
 		PutMatchByte(-distance & 0xFF);
-		PutMatchByte(((-distance >> (8 - 3)) & 0xF8) | ((length - 2) & 7));
 	}
-	else //if (length >= 3)
+	else //if (length >= 10)
 	{
 		PutDescriptorBit(false);
 		PutDescriptorBit(true);
-		PutMatchByte(-distance & 0xFF);
 		PutMatchByte((-distance >> (8 - 3)) & 0xF8);
-		PutMatchByte(length - 1);
+		PutMatchByte(-distance & 0xFF);
+		PutMatchByte(length - 9);
 	}
 }
 
@@ -95,27 +93,27 @@ static unsigned int GetMatchCost(size_t distance, size_t length, void *user)
 		return 8 + 2 + 2;	// Offset byte, length bits, descriptor bits
 	else if (length >= 3 && length <= 9)
 		return 16 + 2;		// Offset/length bytes, descriptor bits
-	else if (length >= 3)
+	else if (length >= 10)
 		return 24 + 2;		// Offset/length bytes, descriptor bits
 	else
 		return 0; 		// In the event a match cannot be compressed
 }
 
-static CLOWNLZSS_MAKE_FUNCTION(FindMatchesKosinski, unsigned char, 0x100, 0x2000, 8 + 1, DoLiteral, GetMatchCost, DoMatch)
+static CLOWNLZSS_MAKE_FUNCTION(FindMatchesKosinskiPlus, unsigned char, 0x100 + 8, 0x2000, 8 + 1, DoLiteral, GetMatchCost, DoMatch)
 
-unsigned char* KosinskiCompress(unsigned char *data, size_t data_size, size_t *compressed_size)
+unsigned char* KosinskiPlusCompress(unsigned char *data, size_t data_size, size_t *compressed_size)
 {
 	output_stream = MemoryStream_Init(0x100);
 	match_stream = MemoryStream_Init(0x10);
 	descriptor_bits_remaining = TOTAL_DESCRIPTOR_BITS;
 
-	FindMatchesKosinski(data, data_size, NULL);
+	FindMatchesKosinskiPlus(data, data_size, NULL);
 
 	// Terminator match
 	PutDescriptorBit(false);
 	PutDescriptorBit(true);
-	PutMatchByte(0x00);
 	PutMatchByte(0xF0);
+	PutMatchByte(0x00);
 	PutMatchByte(0x00);
 
 	FlushData();
