50dec33667877c1e274ce29d041d522bcad0f929
Clownacy <Clownacy@users.noreply.github.com>
2018-12-13 21:29:11 +0000

    Added usage print and a module size option

--- main.c
+++ main.c
@@ -20,7 +20,7 @@ typedef struct Mode
 	unsigned char* (*moduled_function)(unsigned char *data, size_t data_size, size_t *compressed_size, size_t module_size);
 } Mode;
 
-Mode modes[] = {
+static Mode modes[] = {
 	{"-ch", "out.cham", ChameleonCompress, "out.chamm", ModuledChameleonCompress},
 	{"-c", "out.comp", ComperCompress, "out.compm", ModuledComperCompress},
 	{"-k", "out.kos", KosinskiCompress, "out.kosm", ModuledKosinskiCompress},
@@ -28,6 +28,29 @@ Mode modes[] = {
 	{"-r", "out.rock", RocketCompress, "out.rockm", ModuledRocketCompress},
 	{"-s", "out.sax", SaxmanCompress, "out.saxm", ModuledSaxmanCompress},
 };
+
+static void PrintUsage(void)
+{
+	printf(
+	"Clownacy's compression tool thingy\n"
+	"\n"
+	"Usage: tool [options] [in-filename] [out-filename]"
+	"\n"
+	"Options:\n"
+	"\n"
+	" Format:\n"
+	"  -ch    Compress in Chameleon format\n"
+	"  -c     Compress in Comper format\n"
+	"  -k     Compress in Kosinski format\n"
+	"  -kp    Compress in Kosinski+ format\n"
+	"  -r     Compress in Rocket format\n"
+	"  -s     Compress in Saxman format\n"
+	"\n"
+	" Misc:\n"
+	"  -m[=MODULE_SIZE]  Compresses into modules\n"
+	"                    MODULE_SIZE controls the module size (defaults to 0x1000)\n"
+	);
+}
 
 int main(int argc, char *argv[])
 {
@@ -38,14 +61,40 @@ int main(int argc, char *argv[])
 	char *in_filename = NULL;
 	char *out_filename = NULL;
 	bool moduled = false;
+	size_t module_size = 0x1000;
 
 	for (int i = 0; i < argc; ++i)
 	{
 		if (argv[i][0] == '-')
 		{
-			if (!strcmp(argv[i], "-m"))
+			if (!strcmp(argv[i], "-h") || !strcmp(argv[i], "--help"))
+			{
+				PrintUsage();
+			}
+			else if (!strncmp(argv[i], "-m", 2))
 			{
 				moduled = true;
+
+				char *argument = strchr(argv[i], '=');
+
+				if (argument)
+				{
+					char *end;
+					unsigned long result = strtoul(argument + 1, &end, 0);
+
+					if (*end != '\0')
+					{
+						printf("Invalid parameter to -m\n");
+						return -1;
+					}
+					else
+					{
+						module_size = result;
+
+						if (module_size > 0x1000)
+							printf("Warning: the moduled format header does not fully support sizes greater than\n 0x1000 - header will likely be invalid!");
+					}
+				}
 			}
 			else
 			{
@@ -70,11 +119,13 @@ int main(int argc, char *argv[])
 
 	if (!in_filename)
 	{
-		printf("Error: Input file not specified\n");
+		printf("Error: Input file not specified\n\n");
+		PrintUsage();
 	}
 	else if (!mode)
 	{
-		printf("Error: Mode not specified\n");
+		printf("Error: Format not specified\n\n");
+		PrintUsage();
 	}
 	else
 	{
@@ -97,7 +148,7 @@ int main(int argc, char *argv[])
 			unsigned char *compressed_buffer;
 
 			if (moduled)
-				compressed_buffer = mode->moduled_function(file_buffer, file_size, &compressed_size, 0x1000);
+				compressed_buffer = mode->moduled_function(file_buffer, file_size, &compressed_size, module_size);
 			else
 				compressed_buffer = mode->normal_function(file_buffer, file_size, &compressed_size);
 
