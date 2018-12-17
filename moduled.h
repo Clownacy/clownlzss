f8e8a0f70a06e4b924eef2ac47b5d1ceb4f15eb3
Clownacy <Clownacy@users.noreply.github.com>
2018-12-17 21:49:21 +0000

    Removed leftover header file

--- moduled.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <stddef.h>
-
-#include "memory_stream.h"
-
-unsigned char* ModuledCompress(unsigned char *data, size_t data_size, size_t *out_compressed_size, void (*function)(unsigned char *data, size_t data_size, MemoryStream *output_stream), size_t module_size, size_t module_alignment);
